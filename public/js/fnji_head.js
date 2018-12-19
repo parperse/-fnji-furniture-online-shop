ce9fd5784c9ad70624385ce5be3416650d2d1d28
zhangwenchao <761160454@qq.com>
2018-12-19 11:44:41 +0800

    2018/12/18

--- public/js/fnji_head.js
+++ public/js/fnji_head.js
@@ -53,5 +53,24 @@ function over(){
   backdrop.css("display","none");
 }
 $(function(){
-  $()
+  $("#login1").click(function(){
+    var phone= $(".phone").val();
+    var validate= $("#verify-code").val();
+    if(phone!=""&&validate!=""){
+      $.ajax({
+        url:"http://localhost:3000/user/login",
+        type:"post",
+        data:{phone,validate},
+        success:function(res){
+          if(res.ok==1){
+            alert(res.msg);
+            window.location.href="http://127.0.0.1:3000/html/fnji_index.html"
+          }else{
+            alert(res.msg);
+          }
+        }
+      })
+    }
+    
+  })
 })
